c2a84cfe4b447d59adc90b9ef8d5e04b1c5b4faf
KA1ZEN <133351798+TILTENBERG@users.noreply.github.com>
2026-05-11 22:20:29 +0900

    Add LeetCode solution: divide-array-into-arrays-with-max-difference (cpp)

---
+++ LeetCode/divide-array-into-arrays-with-max-difference/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> divideArray(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+       
+        int l=0;
+        int r=nums.size()/3;
+        for(int i=0;i<nums.size();i+=3){
+            if(nums[i+2]-nums[i]>k){
+                return {};
+            }
+            else 
+                ans.push_back({nums[i],nums[i+1],nums[i+2]});
+            
+
+
+        }
+        return ans;
+        
+    }
+};
