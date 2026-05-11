1dade8161cf58e358c8d5fe8dac41e9b156d14a2
KA1ZEN <133351798+TILTENBERG@users.noreply.github.com>
2026-05-11 22:12:51 +0900

    Add LeetCode solution: delete-columns-to-make-sorted-ii (cpp)

---
+++ LeetCode/delete-columns-to-make-sorted-ii/solution.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int n = strs.size();
+        int m = strs[0].size();
+
+        vector<bool> sorted(n - 1, false);
+        int ans = 0;
+
+        for (int col = 0; col < m; ++col) {
+            bool del = false;
+
+            for (int row = 0; row < n - 1; ++row) {
+                if (!sorted[row] && strs[row][col] > strs[row + 1][col]) {
+                    del = true;
+                    break;
+                }
+            }
+
+            if (del) {
+                ans++;
+                continue;
+            }
+            for (int row = 0; row < n - 1; ++row) {
+                if (!sorted[row] && strs[row][col] < strs[row + 1][col]) {
+                    sorted[row] = true;
+                }
+            }
+        }
+        return ans;
+    }
+};
