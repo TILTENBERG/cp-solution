d8303aec0afc9aafbd1ebaa89c46432f34868877
KA1ZEN <133351798+TILTENBERG@users.noreply.github.com>
2026-05-11 22:25:13 +0900

    Add LeetCode solution: minimum-number-of-operations-to-make-elements-in-array-distinct (cpp)

---
+++ LeetCode/minimum-number-of-operations-to-make-elements-in-array-distinct/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        int ans=0;
+        vector<bool> seen(128);
+
+        for(int i=nums.size()-1;i>=0;--i){
+            if(seen[nums[i]]){
+                return i/3+1;
+            }
+            seen[nums[i]]=true;
+        }
+        return 0;
+
+        
+    }
+};
