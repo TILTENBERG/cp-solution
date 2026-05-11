fc8e32ecd3af7984925dcf1a95581cf4691089a8
KA1ZEN <133351798+TILTENBERG@users.noreply.github.com>
2026-05-11 22:11:08 +0900

    Add LeetCode solution: number-of-ways-to-paint-n-3-grid (cpp)

---
+++ LeetCode/number-of-ways-to-paint-n-3-grid/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numOfWays(int n) {
+        const int MOD = 1000000007;
+        long long A = 6;
+        long long B = 6;
+        for (int i = 2; i <= n; ++i) {
+            long long newA = (3 * A + 2 * B) % MOD;
+            long long newB = (2 * A + 2 * B) % MOD;
+            A = newA;
+            B = newB;
+        }
+        return (A + B) % MOD;
+    }
+};
