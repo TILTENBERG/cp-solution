a2eba17662210605213aff37696ca14f1259ac54
KA1ZEN <133351798+TILTENBERG@users.noreply.github.com>
2026-05-11 22:20:23 +0900

    Add LeetCode solution: partition-array-such-that-maximum-difference-is-k (cpp)

---
+++ LeetCode/partition-array-such-that-maximum-difference-is-k/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int partitionArray(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int mn=nums[0];
+        int ans=1;
+        for(int i=1;i<nums.size();++i){
+            if(nums[i]-mn>k){
+                ans++;
+                mn=nums[i];        
+            }
+
+        }
+        return ans;
+    
+
+        
+    }
+};
