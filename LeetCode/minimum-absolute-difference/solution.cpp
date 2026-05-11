85e501e4f1f072f513327cfae405f0bcd707a5db
KA1ZEN <133351798+TILTENBERG@users.noreply.github.com>
2026-05-11 22:10:32 +0900

    Add LeetCode solution: minimum-absolute-difference (cpp)

---
+++ LeetCode/minimum-absolute-difference/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+        int size = arr.size();
+        int diff =  INT_MAX;
+        for(int i=1;i<size;++i){
+            diff = min(diff, arr[i]-arr[i-1]);
+        }
+        vector<vector<int>> ans;
+        for(int i = 1;i<size;++i){
+            if(arr[i]-arr[i-1] == diff) ans.push_back({arr[i-1],arr[i]});
+        }
+        return ans;
+
+        
+    }
+};
